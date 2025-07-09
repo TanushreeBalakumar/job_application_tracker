8b36bed44839977ec70179b2365f50647c0b8f9d
TanushreeBalakumar <tanushreebalakumar@gmail.com>
2025-07-09 19:34:48 +0530

    feat(ui): updated  login page layout and fixed status bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:job_application_tracker/pages/login_page.dart';
 
 
 void main() {
+   WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
+  SystemChrome.setSystemUIOverlayStyle(
+    SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+    ),
+  );
+
   runApp(const MyApp());
 }
 
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -11,53 +11,128 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: SafeArea(
         child: Column( 
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             //Image
-            Image.asset('login.jpg',
-            width: 350,
-            height: 350,),
-            //Login
-            Text(
-              'Login',
-              style: TextStyle(color: const Color.fromARGB(255, 3, 9, 74), fontWeight: FontWeight.bold, fontSize: 24,),
-              textAlign: TextAlign.start, 
+            Image.asset('assets/login.jpg',
+            width: 400,
+            height: 300,
             ),
 
             Container(
-              decoration: BoxDecoration(
-                color: Colors.indigo[50],
-                borderRadius: BorderRadius.circular(12)
-              ),
-              
               padding: EdgeInsets.all(12),
-              child: Row(
+              width: 350,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Icon(Icons.alternate_email_rounded),
-                  SizedBox(width: 10),
-                  Text('Email ID')
-                ]
-              )
-            ),
-            SizedBox(height: 14),
-            Container(
-              decoration: BoxDecoration(
-                color: Colors.indigo[50],
-                borderRadius: BorderRadius.circular(12)
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      'Login',
+                      style: TextStyle(color: const Color.fromARGB(255, 3, 9, 74), fontWeight: FontWeight.bold, fontSize: 30,), 
+                    ),
+                  ),
+
+                  TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Email ID', prefixIcon: Icon(Icons.alternate_email_rounded),
+                    ),
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Password', prefixIcon: Icon(Icons.lock_outline_rounded), 
+                      suffix: TextButton(
+                        onPressed:() {},
+                        child: Text('Forget?',
+                        style: TextStyle(
+                          color: Colors.blueAccent[400]),
+                        ),
+                     ),
+                    ),
+                  ),
+                ],
               ),
-              
-              padding: EdgeInsets.all(12),
-              child: Row(
-                children: [
-                  Icon(Icons.lock_outline_rounded),
-                  SizedBox(width: 10),
-                  Text('Password'),
-                  SizedBox(width: 40),
-                  Text('Forgot?', style: TextStyle(color: Colors.blue),)
-                ]
-              )
             ),
+            SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blueAccent[400], 
+                padding: const EdgeInsets.symmetric(horizontal: 140, vertical: 10),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(14),
+                ),
+              ),
+              child: const Text(
+                'Login',
+                style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 18),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text('Or, login with...'),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                OutlinedButton.icon(
+                  onPressed: () {},
+                  label: Icon(Icons.facebook, size: 30,),
+                  style: OutlinedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    padding: const EdgeInsets.symmetric(horizontal: 35, vertical: 12),
+                    side: const BorderSide(color: Colors.black45),
+                  )
+                ),
+
+                OutlinedButton.icon(
+                  onPressed: () {},
+                  label: Icon(Icons.facebook, size: 30,),
+                  style: OutlinedButton.styleFrom(
+                    shape: RoundedRectangleBorder( 
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    padding: const EdgeInsets.symmetric(horizontal: 35, vertical: 12),
+                    side: const BorderSide(color: Colors.black45),
+                  )
+                ),
+
+                OutlinedButton.icon(
+                  onPressed: () {},
+                  label: Icon(Icons.apple, size: 30, color: Colors.black,),
+                  style: OutlinedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    padding: const EdgeInsets.symmetric(horizontal: 35, vertical: 12),
+                    side: const BorderSide(color: Colors.black45),
+                  )
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 10,
+            ),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('New to JobApplicationTracker?'),
+                TextButton(onPressed: () {}, 
+                child: Text('Register', style: TextStyle(color: const Color.fromARGB(255, 11, 70, 172), fontWeight: FontWeight.bold),)),
+              ],
+            ),
+            
           ],
         ),
       ),
